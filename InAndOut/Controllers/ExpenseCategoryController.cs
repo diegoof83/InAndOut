25ce038589d659dcd89e17cc829537f579deb53b
Diego Ferreira <diegoof@gmail.com>
2021-05-07 14:55:48 -0400

    Added new functionaliry Expense Category

---
+++ InAndOut/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,109 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ExpenseCategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //GET
+        public IActionResult Index()
+        {
+            IEnumerable<ExpenseCategory> categories = _dbContext.ExpenseCategories;
+            return View(categories);
+        }
+
+        //GET_CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST_CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExpenseCategory newExpenseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ExpenseCategories.Add(newExpenseCategory);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newExpenseCategory);
+        }
+
+        //GET-DELETE
+        public IActionResult Delete(int? id)
+        {
+            return this.Get(id);
+        }
+
+        //POST_DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if ((id == null) || (id == 0))
+            {
+                return NotFound();
+            }
+
+            ExpenseCategory category = _dbContext.ExpenseCategories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.ExpenseCategories.Remove(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET-UPDATE
+        public IActionResult Update(int? Id)
+        {
+            return this.Get(Id);
+        }
+
+        //POST_UPDATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ExpenseCategory category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ExpenseCategories.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        private IActionResult Get(int? id)
+        {
+            if ((id == null) || (id == 0))
+            {
+                return NotFound();
+            }
+
+            ExpenseCategory category = _dbContext.ExpenseCategories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+    }
+}
---
+++ InAndOut/Migrations/20210507133952_addExpenseCategoryToDataBase.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class addExpenseCategoryToDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lender",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Borrower",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseCategories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseCategories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lender",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Borrower",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
